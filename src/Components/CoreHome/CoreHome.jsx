d0a5f4d58425a73694aba01022485cd30686896c
16ur <akuseruuu@protonmail.com>
2024-08-22 22:42:12 +0200

    New png file and some minor changes

--- src/Components/CoreHome/CoreHome.jsx
+++ src/Components/CoreHome/CoreHome.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./CoreHome.css";
-import smtp_big from "../../assets/smtp-big.png";
+import smtp from "../../assets/smtp-big.png";
+import smtp_evan from "../../assets/smtp-sans.png";
 
 const CoreHome = () => {
   return (
     <div className="corehome">
-      <h1>CoreHome</h1>
-      <img src={smtp_big} alt="Logo" className="logoBig" />
+      <img src={smtp_evan} alt="Logo" className="logoCore" />
+      <h1 className="titleCore">
+        Le blog qui parle de <span style={{ color: "#0f46d8" }}>claviers</span>.
+      </h1>
     </div>
   );
 };
